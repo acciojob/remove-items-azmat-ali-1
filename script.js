2c538b78069a9361150594a0a688b78b0413284e
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-20 04:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const item = document.getElementById("colorSelect");
+const btn = document.querySelector("form input");
+
+function fun(){
+    item.remove(item.ariaSelectedindex);
+}
+btn.addEventListener("click",fun);
